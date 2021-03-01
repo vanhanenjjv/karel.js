77855b594845199e1dc91ee9dd8610b99cc0b41b
Janne Vanhanen <janne.vanhanen@outlook.com>
2021-03-01 17:01:59 +0200

    Dump dump

---
+++ craco.config.js
@@ -0,0 +1,18 @@
+// const path = require('path');
+// const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+// module.exports = {
+//   webpack: {
+//     configure: {
+//       plugins: [
+//         new MonacoWebpackPlugin({
+//           languages: ['typescript', 'javascript']
+//         })
+//       ],
+//       output: {
+//         path: path.resolve(__dirname, 'build'),
+//         filename: 'bundle.js'
+//       }
+//     }
+//   }  
+// };
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
+import Editor from '@monaco-editor/react';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { DesktopProvider, Desktop } from 'desktop.js'
+
+
+function App() { 
+
+  const [source, setSource] = React.useState<string | undefined>('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <DesktopProvider>
+      <Desktop.Shortcut 
+        name="Karel" 
+        window={{ 
+          title: "yee",
+          content: (
+            <Editor 
+              language="typescript"
+              theme="vs-dark"
+              value={source}
+              onChange={setSource} /> 
+          )
+        }} />
+    </DesktopProvider>
+  )
 }
+
 
 export default App;
